2065d0a7ce24d5b9f589f77d846b345678eb2e2f
Gyles Batara and Hansen Hansen <gyles@>
2017-09-12 10:02:19 -0700

    Add jQuery and js elements

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("div#click1").click(function() {
+    $("#click1 p").toggleClass("highlight");
+  });
+
+  $("div#click2").click(function() {
+    $("#click2 p").toggleClass("highlight");
+  });
+
+  $("div#click3").click(function() {
+    $("#click3 p").toggleClass("highlight");
+  });
+});
